f9c792c3bf07ffa3e0b7ce888cf54c8ee7aa9f11
Chandan <cmahato2000@gmail.com>
2022-07-16 20:17:13 +0545

    css animations

--- soap bubble animation/soap.js
+++ soap bubble animation/soap.js
@@ -1,12 +1,51 @@
-var BackgroundDetails = document.querySelector(".soapBall").style.background;
+var BackgroundDetails = document.querySelector(".soapBall1").style.background;
+var BackgroundDetails = document.querySelector(".soapBall2").style.background;
+var BackgroundDetails = document.querySelector(".soapBall3").style.background;
+var BackgroundDetails = document.querySelector(".soapBall4").style.background;
+var audio = new Audio("bubbleBrustSound.wav");
 
-function brustBubble(){
-    document.querySelector(".soapBall").style.border = "2px dashed rgb(0, 183, 255)";
-    document.querySelector(".soapBall").style.background="none";
+function brustBubble1(){
+    document.querySelector(".soapBall1").style.border = "2px dashed #81e8f6";
+    document.querySelector(".soapBall1").style.background="none";
     setInterval(() => {
-        document.querySelector(".soapBall").style.border = "none";
+        document.querySelector(".soapBall1").style.border = "none";
     }, 100);
+    audio.play();
     setInterval(() => {
-        document.querySelector(".soapBall").style.background=BackgroundDetails;
+        document.querySelector(".soapBall1").style.background=BackgroundDetails;
+    }, 5000);
+    
+}
+function brustBubble2(){
+    document.querySelector(".soapBall2").style.border = "2px dashed #81e8f6";
+    document.querySelector(".soapBall2").style.background="none";
+    setInterval(() => {
+        document.querySelector(".soapBall2").style.border = "none";
+    }, 100);
+    audio.play();
+    setInterval(() => {
+        document.querySelector(".soapBall2").style.background=BackgroundDetails;
+    }, 5000);
+}
+function brustBubble3(){
+    document.querySelector(".soapBall3").style.border = "2px dashed #81e8f6";
+    document.querySelector(".soapBall3").style.background="none";
+    setInterval(() => {
+        document.querySelector(".soapBall3").style.border = "none";
+    }, 100);
+    audio.play();
+    setInterval(() => {
+        document.querySelector(".soapBall3").style.background=BackgroundDetails;
+    }, 5000);
+}
+function brustBubble4(){
+    document.querySelector(".soapBall4").style.border = "2px dashed #81e8f6";
+    document.querySelector(".soapBall4").style.background="none";
+    setInterval(() => {
+        document.querySelector(".soapBall4").style.border = "none";
+    }, 100);
+    audio.play();
+    setInterval(() => {
+        document.querySelector(".soapBall4").style.background=BackgroundDetails;
     }, 5000);
 }
